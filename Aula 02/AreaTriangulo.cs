27e15c66f5c20d3763313ac0eef682636e3f5034
Arnold Rodrigues <arnoldrodrigues11@gmail.com>
2021-01-17 00:27:42 +0000

    Atualização de arquivos.

--- Aula 02/AreaTriangulo.cs
+++ Aula 02/AreaTriangulo.cs
@@ -1,7 +1,16 @@
 using System;
 
 class MainClass {
-  public static void Main(string[], args) {
-
+  public static void Main(string[] args) {
+    double bAse;
+    double altura;
+    double area;
+    Console.WriteLine("Insira a base do triângulo");
+    bAse = double.Parse(Console.ReadLine());
+    Console.WriteLine("Insira a altura do triângulo");
+    altura = double.Parse(Console.ReadLine());
+    
+    area = (altura*bAse)/2;
+    Console.WriteLine(area);
   }
 }
